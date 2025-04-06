1b38f066f8e2e7a25a3390e2d21fafcd9dec7ca9
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-04-06 17:55:03 +0700

    test login

--- src/modules/auth/auth.page.jsx
+++ src/modules/auth/auth.page.jsx
@@ -1,11 +1,43 @@
-import LoginForm from './components/auth.form';
-import { Container, Typography } from '@mui/material';
+import LoginForm from "./components/auth.form";
+import { Box, Container, Typography, useMediaQuery } from "@mui/material";
 
 export default function AuthPage() {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  console.log("siiiii", isMobile)
   return (
-    <Container maxWidth="sm" sx={{ mt: 8 }}>
-      <Typography variant="h4" gutterBottom>Login</Typography>
-      <LoginForm />
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+        width: "100vw",
+      }}
+    >
+      <Box
+        sx={{
+          display: isMobile ? "none" : "block",
+          width: isMobile ? "0%" : "40%",
+          height: "100%",
+        }}
+      >
+        <img src="login_form.svg" width="100%" height="100%" alt="" />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: isMobile ? "100%" : "60%",
+          height: "100%",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <LoginForm />
+      </Box>
+    </Box>
   );
 }
